83ee4d95f0f191734b20de9fbfea89281a0b68f4
Evgeny Cherkashin <eugeneai@irnok.net>
2022-07-06 07:51:49 +0800

    Tried to implement the conversion, failed due to library.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fileinput import filename
+from numpy import dtype
+import numpy
 from pandas import DataFrame, read_excel
 import os.path
+import math
+
+import pyproj
 
 DIR = r"E:\RC\16 июня 2016 работа\Data " \
     r"(D)\Mazaeva disk D 2020\Articles 2021август\taylor " \
@@ -11,7 +16,9 @@ DIR = "./"
 
 def convert(filexls,
         filecsv = None, 
-        sheetname="Лист1"):
+        sheetname="Лист1",
+        azimuthbase = 0,
+        clockwise = True):
 
     if filecsv is None:
         filecsv = filexls
@@ -25,12 +32,40 @@ def convert(filexls,
     lon = coords.lon[0]
     print(coords.columns)
     coords.drop(columns = ['lat', 'lon'])
+    x = coords.x.to_numpy(dtype=float)
+    y = coords.y.to_numpy(dtype=float)
+    d = numpy.sqrt(x**2 + x**2)
+    a = numpy.arctan(x/y)
+    a[a == numpy.nan] = 0.0
+    a = numpy.degrees(a) + azimuthbase
+    if not clockwise:
+        a = - a
     c = DataFrame({
         'x': coords.x,
         "y": coords.y,
-        "z": coords.z
+        "z": coords.z,
+        'd': d,
+        'a': a
     })
-    print(c)
+    d = DataFrame({
+        'd': d,
+        'a': a
+    })
+    g=pyproj.Geod(ellps='WGS84')
+    l = len(a)
+    print(len(a),len(d),l)
+    _lon = numpy.array([lon]*l)
+    _lat = numpy.array([lat]*l)
+    _ = [_lon, _lat, a, d]
+    print([len(x) for x in _])
+    lo, la, ba = g.fwd(*_)
+    r = DataFrame({
+        'n': coords.n,
+        'lat': la,
+        'lon': lo,
+    })
+    print(r)
+    return r
 
 if __name__=="__main__":
     convert("rassv(2008)")
